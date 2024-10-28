b9709936de9442e1565cb1e383a3263ce8bdf719
Franck Silvestre <franck.silvestre@gmail.com>
2024-10-28 18:03:11 +0100

    fix #2.3 Modification de la fonction Element_Plus_Frequent

--- src/tableaux_entiers_util.adb
+++ src/tableaux_entiers_util.adb
@@ -42,15 +42,15 @@ package body Tableaux_Entiers_Util is
       if un_tableau.nbElements = 0 then
          raise ERREUR_TABLEAU_VIDE;
       end if;
-      i := 1;
-      while i <= un_tableau.nbElements loop
+      i := un_tableau.nbElements;
+      while i >= 1 loop
          element_Courant := un_tableau.tableau (i);
          occ_Element_Courant := Nb_Occurences (un_tableau, element_Courant);
          if occ_Element_Courant > occ_Max then
             occ_Max     := occ_Element_Courant;
             element_Max := element_Courant;
          end if;
-         i := i + 1;
+         i := i - 1;
       end loop;
       return element_Max;
    end Element_Plus_Frequent;
